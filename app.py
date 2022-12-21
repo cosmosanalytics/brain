d12e8b0c5d28e4cb024ced710d4b47994b40c2f8
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2022-12-21 14:50:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ st.title('Brain Network')
 def loadData():
     matrix = pd.read_csv('matrix.csv', index_col = 0)
     colorlist = np.array(pd.read_csv('colorlist.csv', index_col = 0)['0'])
-#     colornumbs = np.array(pd.read_csv('colornumbs.csv', index_col = 0)[1])
-#     lineList = np.array(pd.read_csv('lineList.csv', index_col = 0)[1])
-#     sublist = np.array(pd.read_csv('sublist.csv', index_col = 0)[1]) 
+    colornumbs = np.array(pd.read_csv('colornumbs.csv', index_col = 0)['0'])
+    lineList = np.array(pd.read_csv('lineList.csv', index_col = 0)['0'])
+    sublist = np.array(pd.read_csv('sublist.csv', index_col = 0)['0']) 
     
-#     matrix.columns = lineList
-#     matrix.index = lineList
-    return matrix, colorlist#, colornumbs, lineList, sublist
+    matrix.columns = lineList
+    matrix.index = lineList
+    return matrix, colorlist, colornumbs, lineList, sublist
 
-matrix, colorlist = loadData()
+matrix, colorlist, colornumbs, lineList, sublist = loadData()
 st.write(matrix)
 st.write(colorlist)
 
