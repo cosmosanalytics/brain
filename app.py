55d731a3b92312e1cc803e2aecadb27efe07c2e8
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2022-12-21 14:44:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ st.title('Brain Network')
 @st.cache
 def loadData():
     matrix = pd.read_csv('matrix.csv', index_col = 0)
-#     colorlist = np.array(dataiku.Dataset("colorlist").get_dataframe().set_index('col_0')['col_1'])
-#     colornumbs = np.array(dataiku.Dataset("colornumbs").get_dataframe().set_index('col_0')['col_1'])
-#     lineList = np.array(dataiku.Dataset("lineList").get_dataframe().set_index('col_0')['col_1'])
-#     sublist = np.array(dataiku.Dataset("sublist").get_dataframe().set_index('col_0')['col_1']) 
+    colorlist = np.array(pd.read_csv('colorlist', index_col = 0)['col_1'])
+    colornumbs = np.array(pd.read_csv('colornumbs', index_col = 0)['col_1'])
+    lineList = np.array(pd.read_csv('lineList', index_col = 0)['col_1'])
+    sublist = np.array(pd.read_csv('sublist', index_col = 0)['col_1']) 
     
-#     matrix.columns = lineList
-#     matrix.index = lineList
-    
-    return matrix#, colorlist, colornumbs, lineList, sublist
-matrix = loadData()
+    matrix.columns = lineList
+    matrix.index = lineList
+    return matrix, colorlist, colornumbs, lineList, sublist
+
+matrix, colorlist, colornumbs, lineList, sublist = loadData()
 st.write(matrix)
 
