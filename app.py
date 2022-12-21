bd1f1cdc724789db38c1956b4f45d8c2fab34427
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2022-12-21 14:39:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ import numpy as np
 st.title('Brain Network')
 
 @st.cache
-def load_data():
-
-    return data
+def loadData():
+    matrix = pd.read_csv('matrix.csv', index_col = 0)
+#     colorlist = np.array(dataiku.Dataset("colorlist").get_dataframe().set_index('col_0')['col_1'])
+#     colornumbs = np.array(dataiku.Dataset("colornumbs").get_dataframe().set_index('col_0')['col_1'])
+#     lineList = np.array(dataiku.Dataset("lineList").get_dataframe().set_index('col_0')['col_1'])
+#     sublist = np.array(dataiku.Dataset("sublist").get_dataframe().set_index('col_0')['col_1']) 
+    
+#     matrix.columns = lineList
+#     matrix.index = lineList
+    
+    return matrix#, colorlist, colornumbs, lineList, sublist
 
 
