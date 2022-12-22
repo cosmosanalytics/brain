8efed793617897260b0e5a7faad32e4c00458803
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2022-12-21 20:04:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,16 +30,16 @@ def defineG(matrix, threshold, Nodes, Links):
     G.remove_edges_from(list(nx.selfloop_edges(G)))
     return G
 
-def centrality_calc(G, lineList):
-    G_distance_dict = {(e1, e2): 1 / abs(weight) for e1, e2, weight in G.edges(data='weight')}
-    nx.set_edge_attributes(G, G_distance_dict, 'distance')
-    closeness = pd.Series(nx.closeness_centrality(G, distance='distance')); closeness.index = lineList
-    betweenness = pd.Series(nx.betweenness_centrality(G, weight='distance', normalized=True)); betweenness.index = lineList 
-    eigen = pd.Series(nx.eigenvector_centrality(G, weight='weight')); eigen.index = lineList 
-    pagerank = pd.Series(nx.pagerank(G, weight='weight')); pagerank.index = lineList 
-    clustering = pd.Series(nx.clustering(G, weight='weight')); clustering.index = lineList 
-    mean_clutering = nx.average_clustering(G, weight='weight') 
-    return closeness, betweenness, eigen, pagerank, clustering, mean_clutering
+# def centrality_calc(G, lineList):
+#     G_distance_dict = {(e1, e2): 1 / abs(weight) for e1, e2, weight in G.edges(data='weight')}
+#     nx.set_edge_attributes(G, G_distance_dict, 'distance')
+#     closeness = pd.Series(nx.closeness_centrality(G, distance='distance')); closeness.index = lineList
+#     betweenness = pd.Series(nx.betweenness_centrality(G, weight='distance', normalized=True)); betweenness.index = lineList 
+#     eigen = pd.Series(nx.eigenvector_centrality(G, weight='weight')); eigen.index = lineList 
+#     pagerank = pd.Series(nx.pagerank(G, weight='weight')); pagerank.index = lineList 
+#     clustering = pd.Series(nx.clustering(G, weight='weight')); clustering.index = lineList 
+#     mean_clutering = nx.average_clustering(G, weight='weight') 
+#     return closeness, betweenness, eigen, pagerank, clustering, mean_clutering
 
 def brainNX(G, colorlist, colornumbs, lineList, sublist):
     strength = G.degree(weight='weight')
@@ -76,13 +76,13 @@ with col1:
     Links = st.multiselect('Select Link(s)', list(permutations(lineList, 2)))
     threshold = st.slider('Threshold', 0.0, 1.0, 0.0)
     G = defineG(matrix, threshold, Nodes, Links)
-    closeness, betweenness, eigen, pagerank, clustering, mean_clutering = centrality_calc(G,lineList)   
-    fig, ax = plt.subplots(figsize=(20, 3)); ax = closeness.sort_values(ascending=False).plot.bar(); ax.set_title('Closeness'); st.pyplot(fig)  
-    fig, ax = plt.subplots(figsize=(20, 3)); ax = betweenness.sort_values(ascending=False).plot.bar(); ax.set_title('Betweenness'); st.pyplot(fig) 
-    fig, ax = plt.subplots(figsize=(20, 3)); ax = eigen.sort_values(ascending=False).plot.bar(); ax.set_title('Eigen'); st.pyplot(fig)
-    fig, ax = plt.subplots(figsize=(20, 3)); ax = pagerank.sort_values(ascending=False).plot.bar(); ax.set_title('Pagerank'); st.pyplot(fig) 
+#     closeness, betweenness, eigen, pagerank, clustering, mean_clutering = centrality_calc(G,lineList)   
+#     fig, ax = plt.subplots(figsize=(20, 3)); ax = closeness.sort_values(ascending=False).plot.bar(); ax.set_title('Closeness'); st.pyplot(fig)  
+#     fig, ax = plt.subplots(figsize=(20, 3)); ax = betweenness.sort_values(ascending=False).plot.bar(); ax.set_title('Betweenness'); st.pyplot(fig) 
+#     fig, ax = plt.subplots(figsize=(20, 3)); ax = eigen.sort_values(ascending=False).plot.bar(); ax.set_title('Eigen'); st.pyplot(fig)
+#     fig, ax = plt.subplots(figsize=(20, 3)); ax = pagerank.sort_values(ascending=False).plot.bar(); ax.set_title('Pagerank'); st.pyplot(fig) 
 with col2:   
-    fig, ax = plt.subplots(figsize=(20, 3)); ax = clustering.sort_values(ascending=False).plot.bar(); ax.set_title('Clustering, average='+str(mean_clutering)); st.pyplot(fig) 
+#     fig, ax = plt.subplots(figsize=(20, 3)); ax = clustering.sort_values(ascending=False).plot.bar(); ax.set_title('Clustering, average='+str(mean_clutering)); st.pyplot(fig) 
     brainNX(G, colorlist, colornumbs, lineList, sublist)
 
 
