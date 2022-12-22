7b66549119079ca273bf02205bcba174927ead40
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2022-12-21 22:37:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ matrix, colorlist, colornumbs, lineList, sublist, refDF = loadData()
 def color_colorlist(val):
     color = val
     return f'background-color: {color}'
-st.dataframe(refDF[refDF['lineList'].isin(nds)].style.applymap(color_colorlist, subset=['colorlist']))    
+st.dataframe(refDF.style.applymap(color_colorlist, subset=['colorlist']))    
 col1, col2 = st.columns(2)
 with col1:
     Nodes = st.multiselect('Select Node(s) to Remove', lineList)
