e9ec4737aa476fef52b89e18db99713cb1081198
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2022-12-21 20:29:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,16 +30,16 @@ def defineG(matrix, threshold, Nodes, Links):
     G.remove_edges_from(list(nx.selfloop_edges(G)))
     return G
 
-# def centrality_calc(G, lineList):
-#     G_distance_dict = {(e1, e2): 1 / abs(weight) for e1, e2, weight in G.edges(data='weight')}
-#     nx.set_edge_attributes(G, G_distance_dict, 'distance')
-#     closeness = pd.Series(nx.closeness_centrality(G, distance='distance')); closeness.index = lineList
-#     betweenness = pd.Series(nx.betweenness_centrality(G, weight='distance', normalized=True)); betweenness.index = lineList 
-#     eigen = pd.Series(nx.eigenvector_centrality(G, weight='weight')); eigen.index = lineList 
-#     pagerank = pd.Series(nx.pagerank(G, weight='weight')); pagerank.index = lineList 
-#     clustering = pd.Series(nx.clustering(G, weight='weight')); clustering.index = lineList 
-#     mean_clutering = nx.average_clustering(G, weight='weight') 
-#     return closeness, betweenness, eigen, pagerank, clustering, mean_clutering
+def centrality_calc(G, lineList):
+    G_distance_dict = {(e1, e2): 1 / abs(weight) for e1, e2, weight in G.edges(data='weight')}
+    nx.set_edge_attributes(G, G_distance_dict, 'distance')
+    closeness = pd.Series(nx.closeness_centrality(G, distance='distance')); closeness.index = lineList
+    betweenness = pd.Series(nx.betweenness_centrality(G, weight='distance', normalized=True)); betweenness.index = lineList 
+    eigen = pd.Series(nx.eigenvector_centrality(G, weight='weight')); eigen.index = lineList 
+    pagerank = pd.Series(nx.pagerank(G, weight='weight')); pagerank.index = lineList 
+    clustering = pd.Series(nx.clustering(G, weight='weight')); clustering.index = lineList 
+    mean_clutering = nx.average_clustering(G, weight='weight') 
+    return closeness, betweenness, eigen, pagerank, clustering, mean_clutering
 
 def brainNX(G, colorlist, colornumbs, lineList, sublist):
     strength = G.degree(weight='weight')
@@ -64,13 +64,6 @@ def brainNX(G, colorlist, colornumbs, lineList, sublist):
     nx.draw(G, pos, with_labels=True, width=np.power(edgewidth, 2), edge_color='grey', node_size=normstrengthlist*20000, 
             labels=Convert(lineList), font_color='black', node_color=colornumbs/10, cmap=plt.cm.Spectral, alpha=0.7, font_size=9)
     st.pyplot(fig)
-    
-    fig, ax = plt.subplots(figsize=(20,3))
-    G1 = nx.from_pandas_edgelist(pd.DataFrame({'lineList':lineList, 'sublist':sublist}), 'lineList', 'sublist')
-    pos = nx.spring_layout(G1, scale=5)
-    nx.draw(G1, pos, with_labels=True, edge_color='grey', node_size=normstrengthlist*20000, 
-            labels=Convert(lineList), font_color='black', node_color=colornumbs/10, cmap=plt.cm.Spectral, alpha=0.7, font_size=9)    
-    st.pyplot(fig)
 
 col1, col2 = st.columns(2)
 with col1:
@@ -79,13 +72,13 @@ with col1:
     Links = st.multiselect('Select Link(s)', list(permutations(lineList, 2)))
     threshold = st.slider('Threshold', 0.0, 1.0, 0.0)
     G = defineG(matrix, threshold, Nodes, Links)
-#     closeness, betweenness, eigen, pagerank, clustering, mean_clutering = centrality_calc(G,lineList)   
-#     fig, ax = plt.subplots(figsize=(20, 3)); ax = closeness.sort_values(ascending=False).plot.bar(); ax.set_title('Closeness'); st.pyplot(fig)  
-#     fig, ax = plt.subplots(figsize=(20, 3)); ax = betweenness.sort_values(ascending=False).plot.bar(); ax.set_title('Betweenness'); st.pyplot(fig) 
-#     fig, ax = plt.subplots(figsize=(20, 3)); ax = eigen.sort_values(ascending=False).plot.bar(); ax.set_title('Eigen'); st.pyplot(fig)
-#     fig, ax = plt.subplots(figsize=(20, 3)); ax = pagerank.sort_values(ascending=False).plot.bar(); ax.set_title('Pagerank'); st.pyplot(fig) 
+    closeness, betweenness, eigen, pagerank, clustering, mean_clutering = centrality_calc(G,lineList)   
+    fig, ax = plt.subplots(figsize=(20, 3)); ax = closeness.sort_values(ascending=False).plot.bar(); ax.set_title('Closeness'); st.pyplot(fig)  
+    fig, ax = plt.subplots(figsize=(20, 3)); ax = betweenness.sort_values(ascending=False).plot.bar(); ax.set_title('Betweenness'); st.pyplot(fig) 
+    fig, ax = plt.subplots(figsize=(20, 3)); ax = eigen.sort_values(ascending=False).plot.bar(); ax.set_title('Eigen'); st.pyplot(fig)
+    fig, ax = plt.subplots(figsize=(20, 3)); ax = pagerank.sort_values(ascending=False).plot.bar(); ax.set_title('Pagerank'); st.pyplot(fig) 
 with col2:   
-#     fig, ax = plt.subplots(figsize=(20, 3)); ax = clustering.sort_values(ascending=False).plot.bar(); ax.set_title('Clustering, average='+str(mean_clutering)); st.pyplot(fig) 
+    fig, ax = plt.subplots(figsize=(20, 3)); ax = clustering.sort_values(ascending=False).plot.bar(); ax.set_title('Clustering, average='+str(mean_clutering)); st.pyplot(fig) 
     brainNX(G, colorlist, colornumbs, lineList, sublist)
 
 
