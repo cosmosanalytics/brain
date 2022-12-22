345aeb8be781e2bbdb13266179e9b42f3dcf6e3e
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2022-12-21 23:14:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,8 @@ with col1:
     G = defineG(matrix, threshold, Nodes, Links)
     closeness, betweenness, clustering, mean_clutering = centrality_calc(G,lineList)  
     fig, ax = plt.subplots(figsize=(20, 3)); ax = closeness.sort_values(ascending=False).plot.bar(color=refDF['colorlist']); ax.set_title('Closeness'); st.pyplot(fig)  
-    fig, ax = plt.subplots(figsize=(20, 3)); ax = betweenness.sort_values(ascending=False).plot.bar(); ax.set_title('Betweenness'); st.pyplot(fig) 
-    fig, ax = plt.subplots(figsize=(20, 3)); ax = clustering.sort_values(ascending=False).plot.bar(); ax.set_title('Clustering, average='+str(mean_clutering)); st.pyplot(fig)     
+    fig, ax = plt.subplots(figsize=(20, 3)); ax = betweenness.sort_values(ascending=False).plot.bar(color=refDF['colorlist']); ax.set_title('Betweenness'); st.pyplot(fig) 
+    fig, ax = plt.subplots(figsize=(20, 3)); ax = clustering.sort_values(ascending=False).plot.bar(color=refDF['colorlist']); ax.set_title('Clustering, average='+str(mean_clutering)); st.pyplot(fig)     
 with col2:   
     def color_colorlist(val):
         color = val
