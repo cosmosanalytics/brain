3244f4a8c68509a9c7dd9428253aebe56bac9952
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-02-04 14:03:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ with col2:
         VA = pd.Series(st.text_input('VA NODES TO FOCUS: (RPG2,LT2,LPG8,RPG10)','0.0, 0.0, 0.0, 0.0').split(',')).astype(float)
         MS = pd.Series(st.text_input('MISCELLANEOUS : (RAG1,LAG1)', '0.0, 0.0').split(',')).astype(float)
                            
-        init = SM.append(DMN).append(LIM).append(VIS).append(FP).append(VA).append(MS)
+        init = SM.append(DMN, ignore_index=True).append(LIM, ignore_index=True).append(VIS, ignore_index=True).append(FP, ignore_index=True).append(VA, ignore_index=True).append(MS, ignore_index=True)
  
         if st.button('simulation'):
             iterations = dynBrainNX(G,epsilon,init)
